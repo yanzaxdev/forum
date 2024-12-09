6c9998e76462efdab03d761ed895854d525a97b4
yaniv <yanivzaxdev@gmail.com>
2024-12-09 15:01:25 +0200

    TS version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
