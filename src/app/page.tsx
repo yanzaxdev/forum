42cf1a2175726948479db69aae316f16e36b5fd0
yaniv <yanivzaxdev@gmail.com>
2024-12-09 15:43:22 +0200

    changing env.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       Hello 2
-      {posts.map((post) => {
-        return <div key={post.id}>{post.id}</div>;
-      })}
     </main>
   );
 }
