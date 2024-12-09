f1388ddb44d6fdb1dc09f7cd081ce7d3768c160d
yaniv <yanivzaxdev@gmail.com>
2024-12-09 14:29:24 +0200

    Db Setup completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { db } from "~/server/db";
+
 export default async function Home() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello T3
+      Hello 2
+      {posts.map((post) => {
+        return <div key={post.id}>{post.id}</div>;
+      })}
     </main>
   );
 }
