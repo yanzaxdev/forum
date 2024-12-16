b3c68bd53474a0147f321c30ad7254f2b1ed74cb
yaniv <yanivzaxdev@gmail.com>
2024-12-16 16:10:25 +0200

    Population of db with dummy data completed

--- src/server/db/course_ranking.ts
+++ src/server/db/course_ranking.ts
@@ -1,5 +1,5 @@
 import {sql} from 'drizzle-orm';
-import {decimal, index, integer, pgTableCreator, text, timestamp, varchar,} from 'drizzle-orm/pg-core';
+import {decimal, index, integer, pgTableCreator, text, timestamp, unique, varchar,} from 'drizzle-orm/pg-core';
 
 import {courses} from './courses';
 import {createTable} from './tableCreator';
@@ -12,8 +12,7 @@ export const courseRankings = createTable(
       id: integer('id').primaryKey().generatedByDefaultAsIdentity(),
       courseId: integer('course_id').notNull().references(() => courses.id),
       userId: integer('user_id').notNull().references(() => users.id),
-      gradeAverage:
-          decimal('grade_average', {precision: 5, scale: 2}).default(sql`0`),
+      grade: decimal('grade_average', {precision: 5, scale: 2}).default(sql`0`),
       examDifficulty:
           decimal('exam_difficulty', {precision: 3, scale: 2}).default(sql`0`),
       assignmentDifficulty:
@@ -28,4 +27,6 @@ export const courseRankings = createTable(
     (ranking) => ({
       courseIdIndex: index('course_id_idx').on(ranking.courseId),
       userIdIndex: index('user_id_idx').on(ranking.userId),
+      uniqueCourseUserRanking: unique('unique_course_user_ranking')
+                                   .on(ranking.userId, ranking.courseId)
     }));
