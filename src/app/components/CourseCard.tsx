e3c0f67c61d73f648789aff8b1ddc0770ce3fb9f
yaniv <yanivzaxdev@gmail.com>
2024-12-16 17:06:17 +0200

    Refactoring

---
+++ src/app/components/CourseCard.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { FC, useState, useEffect, useContext } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./Card";
+import { InferSelectModel } from "drizzle-orm";
+import { courses } from "~/server/db/courses";
+import { useLanguage } from "../providers";
+import { useRouter } from "next/navigation";
+type Props = {
+  course: InferSelectModel<typeof courses>;
+};
+
+const CourseCard: FC<Props> = ({ course }) => {
+  /* ----- Context -----*/
+  const { lang, isHeb, t, langParam } = useLanguage();
+
+  /* ----- Variables -----*/
+  const title = lang === "en" ? course.titleEn : course.titleHe;
+  const desc = lang === "en" ? course.descriptionEn : course.descriptionHe;
+  const router = useRouter();
+
+  /* ----- Functions -----*/
+  const onClick = () => router.push(`/courses/${course.id}${langParam}`);
+
+  /* ----- Return -----*/
+  return (
+    <Card
+      dir={`${isHeb ? "rtl" : "ltr"}`}
+      onClick={onClick}
+      className="my-2 transition-colors duration-300 hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-600"
+    >
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          <span>{title}</span>
+          <span className="text-sm text-gray-500">{course.id}</span>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className="mb-2">{desc}</p>
+        <span className="text-blue-500 hover:underline">{t.viewCourse}</span>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CourseCard;
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "~/server/db";
 import { courses } from "~/server/db/schema";
-import CoursesList from "./CoursesList";
 import { serverDetLang } from "../utils/language";
+import CourseCard from "../components/CourseCard";
 
 export default async function CoursesPage({
   searchParams,
@@ -10,13 +10,13 @@ export default async function CoursesPage({
 }) {
   // Fetch courses from the DB
   const allCourses = await db.select().from(courses);
-  const { t, lang } = await serverDetLang(searchParams);
-  console.log({ lang });
+  const { t, lang, isHeb } = await serverDetLang(searchParams);
 
   return (
     <main className="mx-auto max-w-2xl px-4 py-8">
-      <h1 className="mb-4 text-center text-3xl font-bold">{t.courses}</h1>
-      <CoursesList courses={allCourses} />
+      {allCourses.map((course) => (
+        <CourseCard key={course.id} course={course} />
+      ))}
     </main>
   );
 }
