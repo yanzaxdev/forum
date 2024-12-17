53c01008101b28667554f35437ec76fdcde7fcfd
yaniv <yanivzaxdev@gmail.com>
2024-12-18 01:08:36 +0200

    More bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 import { serverDetLang } from "../utils/language";
+import { cn } from "~/lib/utils";
 export const dynamic = "force-dynamic";
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { lang?: string };
-}) {
+interface HomePageProps {
+  searchParams: {
+    lang?: string;
+  };
+}
+
+export default async function HomePage({ searchParams }: HomePageProps) {
   const { t } = await serverDetLang(searchParams);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-4 py-8">
-      <h1 className="mb-4 text-4xl font-bold">{t.welcomeMessage}</h1>
-      <p className="mb-6 max-w-md text-center text-lg">
+    <main
+      dir={t._dir}
+      className={cn(
+        "flex min-h-screen flex-col",
+        "items-center justify-center",
+        "px-4 py-8",
+      )}
+    >
+      <h1 className={cn("text-4xl font-bold", "mb-4")}>{t.welcomeMessage}</h1>
+
+      <p className={cn("text-center text-lg", "max-w-md", "mb-6")}>
         {t.welcomeDescription}
       </p>
     </main>
